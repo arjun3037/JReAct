f7f3f396563d35d008f07b38d99283203d7c96ff
arjun <arjun3037@gmail.com>
2026-08-08 23:06:21 +0530

    Scaffold Spring Boot project with Spring AI dependencies

---
+++ src/main/java/com/jreact/JReactApplication.java
@@ -0,0 +1,12 @@
+package com.jreact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JReactApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JReactApplication.class, args);
+    }
+}
